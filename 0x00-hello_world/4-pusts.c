5bce0d070d29a4ba538350c1616b881b3df7d43d
Imoh Eric <ericimoh@gmail.com>
2022-12-08 06:44:00 -0800

    4-puts

--- 0x00-hello_world/4-pusts.c
+++ 0x00-hello_world/4-pusts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
+ * main - Entry point
  *
- * Return: Always 0 
- **/
-int main (void)
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	puts ("\" programming is like building multilingual puzzle");
+	puts ("\"programming is like building a multilingual puzzle");
 	return (0);
 }
