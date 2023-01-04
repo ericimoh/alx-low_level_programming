4ce960f7c64f74f40b39031916e51803545050f8
Imoh Eric <ericimoh@gmail.com>
2023-01-04 03:24:19 -0800

    2-strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
 * _strlen_recursion - Returns Length of String
-* @s: string
+*  @s: string
 *
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
@@ -13,7 +13,6 @@ int _strlen_recursion(char *s)
 	if (*s != '\0')
 	{
 		return (1 + _strlen_recursion(s + 1));
-
 	}
 
 	return (0);
